a654ee2d33c4de7dd9b38d416045831fbe4c0fa4
NicolasSoulay <soulaynicolas@gmail.com>
2023-11-30 11:09:53 +0100

    dump of the database

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # ProjectManager
+
+## How to start 
+
+- clone this repository in your apache server (git clone https://github.com/NicolasSoulay/ProjectManager)
+- go to the root file of the project, open the terminal and use the command: composer update
+- create a database, and use the .sql file inside /conception to restore it
+- modify the Config class to enable the connection to the database you just restored
+- use your web browser to display the website, now you can either create a new user, or use the one in the database (email: pepito@pepito.com, password: Pepito123)
+
+## TODO
+
+- Project Controller, Task Controller.
---
+++ conception/dump-project_manager-202311301109.sql
@@ -0,0 +1,194 @@
+-- MariaDB dump 10.19  Distrib 10.6.12-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: project_manager
+-- ------------------------------------------------------
+-- Server version	10.6.12-MariaDB-0ubuntu0.22.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `LifeCycle`
+--
+
+DROP TABLE IF EXISTS `LifeCycle`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `LifeCycle` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `label` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `label` (`label`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `LifeCycle`
+--
+
+LOCK TABLES `LifeCycle` WRITE;
+/*!40000 ALTER TABLE `LifeCycle` DISABLE KEYS */;
+INSERT INTO `LifeCycle` VALUES (2,'en cours'),(1,'Non débuté'),(3,'terminé');
+/*!40000 ALTER TABLE `LifeCycle` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Participate`
+--
+
+DROP TABLE IF EXISTS `Participate`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Participate` (
+  `id_Project` int(11) NOT NULL,
+  `id_User` int(11) NOT NULL,
+  PRIMARY KEY (`id_Project`,`id_User`),
+  KEY `id_User` (`id_User`),
+  CONSTRAINT `Participate_ibfk_1` FOREIGN KEY (`id_Project`) REFERENCES `Project` (`id`),
+  CONSTRAINT `Participate_ibfk_2` FOREIGN KEY (`id_User`) REFERENCES `UserAccount` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Participate`
+--
+
+LOCK TABLES `Participate` WRITE;
+/*!40000 ALTER TABLE `Participate` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Participate` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Priority`
+--
+
+DROP TABLE IF EXISTS `Priority`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Priority` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Priority`
+--
+
+LOCK TABLES `Priority` WRITE;
+/*!40000 ALTER TABLE `Priority` DISABLE KEYS */;
+INSERT INTO `Priority` VALUES (1),(2),(3),(4),(5);
+/*!40000 ALTER TABLE `Priority` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Project`
+--
+
+DROP TABLE IF EXISTS `Project`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Project` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT NULL,
+  `id_admin` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_User` (`id_admin`),
+  CONSTRAINT `Project_ibfk_1` FOREIGN KEY (`id_admin`) REFERENCES `UserAccount` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Project`
+--
+
+LOCK TABLES `Project` WRITE;
+/*!40000 ALTER TABLE `Project` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Project` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Task`
+--
+
+DROP TABLE IF EXISTS `Task`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Task` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT NULL,
+  `id_lifeCycle` int(11) NOT NULL,
+  `id_priority` int(11) NOT NULL,
+  `id_project` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_LifeCycle` (`id_lifeCycle`),
+  KEY `id_Priority` (`id_priority`),
+  KEY `id_Project` (`id_project`),
+  KEY `id_User` (`id_user`),
+  CONSTRAINT `Task_ibfk_1` FOREIGN KEY (`id_lifeCycle`) REFERENCES `LifeCycle` (`id`),
+  CONSTRAINT `Task_ibfk_2` FOREIGN KEY (`id_priority`) REFERENCES `Priority` (`id`),
+  CONSTRAINT `Task_ibfk_3` FOREIGN KEY (`id_project`) REFERENCES `Project` (`id`),
+  CONSTRAINT `Task_ibfk_4` FOREIGN KEY (`id_user`) REFERENCES `UserAccount` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Task`
+--
+
+LOCK TABLES `Task` WRITE;
+/*!40000 ALTER TABLE `Task` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Task` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `UserAccount`
+--
+
+DROP TABLE IF EXISTS `UserAccount`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `UserAccount` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `firstName` varchar(100) NOT NULL,
+  `lastName` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `UserAccount`
+--
+
+LOCK TABLES `UserAccount` WRITE;
+/*!40000 ALTER TABLE `UserAccount` DISABLE KEYS */;
+INSERT INTO `UserAccount` VALUES (6,'pepito@pepito.com','$2y$10$mfOpuSRGjsGVx/FfVp2fluX1N66ZSHIN0nxziSRn3wiDihYstS1aS','Pepito','Machiato');
+/*!40000 ALTER TABLE `UserAccount` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'project_manager'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-11-30 11:09:24
